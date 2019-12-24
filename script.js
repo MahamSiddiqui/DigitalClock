a66d5cf5ad6e1cdff4fc7a30f66ae68eab5c0f3c
Maham Siddiqui <49103875+MahamSiddiqui@users.noreply.github.com>
2019-12-24 17:02:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function clock(){
+    let days = document.getElementById('days');
+    let hours =document.getElementById('hours');
+    let minutes =document.getElementById('minutes');
+    let seconds =document.getElementById('seconds');
+
+    let d = new Date().getDay();
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+
+
+}
+let interval = setInterval(clock,1000);
